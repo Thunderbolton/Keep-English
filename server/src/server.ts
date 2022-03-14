f39682d3419846f4fff5c7d7af86cd28705845e3
Adam Bolton <adam_bolton90@hotmail.com>
2022-03-14 19:38:12 +0900

    client and server folder setup

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,12 @@
-const test: string = 'test';
+import express from 'express';
+
+const app = express();
+
+const add = (a: number, b: number): number => a + b;
+
+app.get('/', (req, res) => {
+    console.log(add(5, 5))
+    res.send('start');
+})
+
+app.listen(5000, () => console.log('server running'));
