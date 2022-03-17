a78298b6360da264139959a20b1a0803587dde43
Adam Bolton <adam_bolton90@hotmail.com>
2022-03-17 23:04:28 +0900

    created db.js file and connected to MongoDB

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,10 @@
 import express, { Application, Request, Response } from 'express';
+const dotenv = require('dotenv').config()
+const connectDB = require('../config/db.js')
+const port = process.env.PORT || 5000
+
+
+connectDB()
 
 const app: Application = express();
 
@@ -6,4 +12,4 @@ app.get('/', (req: Request, res: Response) => {
     res.send('start');
 })
 
-app.listen(5000, () => console.log('server running'));
+app.listen(port, () => console.log(`server running on ${port}`));
