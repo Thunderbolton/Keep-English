a228a3d401dbbe00b7262cd4b9dcb64d1e3648e0
Adam Bolton <adam_bolton90@hotmail.com>
2022-04-15 23:03:14 +0900

    added a grid layout for entries collection

--- client/src/components/EntriesCollection.tsx
+++ client/src/components/EntriesCollection.tsx
@@ -1,4 +1,4 @@
-import { Card, CardContent, CardHeader, IconButton } from '@mui/material';
+import { Card, CardContent, CardHeader, Container, Grid, IconButton } from '@mui/material';
 // import { MoreVertIcon } from '@mui/icons-material';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 
@@ -8,9 +8,11 @@ const EntriesCollection = ({entries} : {entries:any}) => {
     return (
       <div>
           <h1>Entries collection</h1>
+          <Container>
+            <Grid container justifyContent="space-evenly" alignItems="center" gap={4}>
 
-          {entries.map((entry:any) => (
-            
+              {entries.map((entry:any) => (
+
               <Card variant='outlined' sx={{ maxWidth: 450, minHeight: 350 }}>
                 <CardHeader 
                   title={entry.text} 
@@ -25,10 +27,14 @@ const EntriesCollection = ({entries} : {entries:any}) => {
                   <li>{entry._id}</li>
                 </CardContent>
               </Card>
+
+              ))}
+
+            </Grid>
+
+          </Container>
           
-            
-            
-          ))}
+          
       </div>
     );
   }
