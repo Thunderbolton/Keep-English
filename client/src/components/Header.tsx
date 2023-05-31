97d48fff400e1e7e0313af6834460798b16af7e2
Adam Bolton <adam_bolton90@hotmail.com>
2023-05-31 23:01:47 +0900

    added user avatar in Header component

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,6 +1,6 @@
 import { SignOut } from './SignOut';
 import { Link } from 'react-router-dom';
-import { Button, Container } from '@mui/material';
+import { Avatar, Button, Container } from '@mui/material';
 import HomeIcon from '@mui/icons-material/Home';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box/Box';
@@ -8,6 +8,7 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import { AuthContext } from "../context/AuthContext";
 import { useContext, useEffect, useState } from 'react';
+import { deepOrange } from '@mui/material/colors';
 
 
 
@@ -23,32 +24,53 @@ const Header = () => {
   const { user } = useContext(AuthContext)
 
   const [welcome, setWelcome] = useState('')
+  const [avatar, setAvatar] = useState(false)
 
   useEffect(() => {
-    if (user)
-    setWelcome(`Welcome back, ${user.name}`)
-    const welcomeTimer = setTimeout(() => {
-        setWelcome(`${user.name}`);
-    }, 3000);
-
-    return () => clearTimeout(welcomeTimer);
+    if (user) {
+      setWelcome(`Welcome back, ${user.name}`);
+      setAvatar(false);
+  
+      const welcomeTimer = setTimeout(() => {
+        setWelcome('');
+        setAvatar(true);
+      }, 4000);
+  
+      return () => clearTimeout(welcomeTimer);
+    } else {
+      setWelcome('');
+      setAvatar(false);
+    }
   }, [user]);
 
   return (
     <>
     <Container maxWidth={false} disableGutters>
-      <Box>
+      <Box display="flex" justifyContent="center">
         <AppBar color='transparent' position='static'>
           <Toolbar>
             <Link to="/">
               <HomeIcon sx={{color: 'green'}} />
             </Link>
-            <Typography variant="h3" sx={{ paddingLeft: '12%', marginLeft: 'auto', fontSize: 'clamp(1rem, 1.7rem, 2rem)' }}><Link to='/' style={{ color: 'inherit', textDecoration: 'inherit'}}>Keep English</Link>
+            <Typography 
+            variant="h3" 
+            sx={{ 
+              // border:'2px solid red', 
+              marginLeft: 'auto', 
+              marginRight: 'auto', 
+              display: 'flex', 
+              alignItems: 'center',
+              justifyContent: 'center',
+              flexGrow: 1, 
+              fontSize: 'clamp(1rem, 1.7rem, 2rem)' }}>
+                <Link to='/' 
+                style={{ color: 'inherit', textDecoration: 'inherit'}}>Keep English</Link>
             </Typography>
             <ul style={{marginLeft: 'auto', display: 'flex', flexWrap: 'nowrap'}}>
             {user && (
             <>  
               <p>{welcome}</p>
+              {avatar ? <Avatar sx={{ bgcolor: deepOrange[500] }}>{user.name.charAt(0).toUpperCase()}</Avatar> : null}
               <Button 
                 sx={{marginLeft: '0.7rem', fontSize: 'clamp(0.2rem, 0.8rem, 1.5rem)'}}  
                 variant="outlined" 
